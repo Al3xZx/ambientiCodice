d0e111a74ee31f7e1a19b0c620f8993fc5ef4355
alessandro <alessandro.molinaro96@gmail.com>
2019-07-23 15:36:32 +0200

    commit esercizio sposta test

---
+++ programmiShell/variEsercizi/sposta.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+#Scrivere un programma che riceva come argomento
+#(da linea di comando) il nome di un file ed il nome di
+#una directory, e sposti il file nella directory:
+#Ad esempio:
+# sposta fileA directoryB
